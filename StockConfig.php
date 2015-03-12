1925e6ba369f9f2d83dd0a6ff645974dd13a4542
takuo39 <taku.united@gmail.com>
2015-03-13 00:53:03 +0900

    定数クラス作成

---
+++ StockConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+class StockConfig{
+	
+	// DataSourceName
+	const dsn = 'mysql:dbname=db_stock;host=localhost';
+	// user
+	const user = 'root';
+	// password
+	const password = 'root';
+}
+
+?>
